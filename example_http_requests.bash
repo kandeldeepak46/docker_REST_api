167fb752ebeee9f006ca70f5b4885799113b5e61
xps <kandeldeepak46@gmail.com>
2020-12-31 21:54:15 +0545

    REST test code and test code in bash is completed

---
+++ example_http_requests.bash
@@ -0,0 +1,22 @@
+#!bin/bash
+#
+#starting the api
+
+python api.py 9999 & sleep 2
+
+#POST predict method
+curl -d '[
+    
+    {"Age": 85, "Sex": "male", "Embarked": "S"},
+    {"Age": 24, "Sex": "female", "Embarked": "C"},
+    {"Age": 3, "Sex": "male", "Embarked": "C"},
+    {"Age": 21, "Sex": "male", "Embarked": "S"}
+    
+    ]' -H 'Content-type : application/json' -X POST http://localhost:9999/predict && echo -e '\n -> predict OK'
+
+#GET method wipe
+curl -X GET http://localhost:9999/wipe && echo -e '\n -> wipe OK'
+
+#GET method train
+
+curl -X GET http://localhost/train && echo -e '\n train OK'
